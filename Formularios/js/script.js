da31b1833df83f764de05fa322fc224249c7d00e
Amancio TC <Amancio.torres.cruz@gmail.com>
2025-08-29 12:47:51 -0600

    [Modifica] se esta modificando el estilo y el js se agregaron los elementos de la tarjeta

--- Formularios/js/script.js
+++ Formularios/js/script.js
@@ -4,13 +4,28 @@ function crearimagen(event){
     console.log(event.target.value);
     elementos.innerHTML = "";
     for(let i = 0; i< event.target.value; i++){
+        const contenedor = document.createElement("div");
+        contenedor.classList.add("contenedor");
+        
         const imagen = new Image();
         imagen.src = "../recursos/pikachu.jpg";
         imagen.classList.add("imagen-nueva");
+        
         const titulo = document.createElement("h2");
         titulo.textContent = imagen.src.substring(imagen.src.lastIndexOf('/')+1);
-        elementos.appendChild(imagen);
-        elementos.appendChild(titulo);
+        
+        const texto = document.createElement("p");
+        texto.textContent = `Lorem ipsum dolor sit amet, 
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+        
+        const boton = document.createElement("button");
+        boton.textContent = `Leer mas`
+
+        contenedor.appendChild(imagen);
+        contenedor.appendChild(titulo);
+        contenedor.appendChild(texto);
+        contenedor.appendChild(boton);
+        elementos.appendChild(contenedor);
     }
     /*
     const imagen = `<img src="recursos/pikachu.jpg"></img>`;

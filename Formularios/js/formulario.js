fef9848aafc025f8bbb5b07cf019c7f6f3411e3c
Amancio <Amancio@gmail.com>
2025-09-08 12:30:44 -0600

    [Modifica] se modifico el archivo de formulario.js para agregar el metodo para cargar la imagen del input tipo file

--- Formularios/js/formulario.js
+++ Formularios/js/formulario.js
@@ -1,7 +1,10 @@
 const formulario_producto_nuevo = document.querySelector("#formulario_producto_nuevo");
+let productos = [];//estoy declarando un arreglo vacio
+let src_imagen_producto = "../recursos/default.png";
 
 class Producto{
-    constructor(id,nombre, decripcion, precio){
+    constructor(id,imagen,nombre, decripcion, precio){
+        this.Imagen = imagen; 
         this.Id = id;
         this.Nombre = nombre;
         this.Descripcion = decripcion;
@@ -9,16 +12,39 @@ class Producto{
     }
     ObtenerDatos(){
         console.log(this.Id);
+        console.log(this.Imagen);
         console.log(this.Nombre);
-        console.log(this.Decripcion);
+        console.log(this.Descripcion);
         console.log(this.Precio);
     }
 }
 
 function AgregarProducto(event){
-    let lectorFormulario = new FormData(formulario_producto_nuevo);
+    let datosFormulario = new FormData(formulario_producto_nuevo);
     //console.log(lectorFormulario);
-    const datos = Object.fromEntries(lectorFormulario.entries());
-    const json = JSON.stringify(datos);
-    console.log(json);
+    const datos = Object.fromEntries(datosFormulario.entries());
+    console.log(datos)
+    if(datos.Nombre !="" && datos.Descripcion !="" && datos.Precio != null){
+        productos.push(new Producto(productos.length+1, src_imagen_producto, datos.Nombre, datos.Descripcion, datos.Precio));
+    
+        /*imptimir el arreglo por medio de un foreach*/
+        productos.forEach(producto => {
+            producto.ObtenerDatos();
+        });
+    }
+    /*const json = JSON.stringify(datos);
+    console.log(datos);**/
+}
+
+function obtenerImagen(event){
+    const file = event.target.files[0];
+    if(file.type === "image/jpeg" || file.type === "image/png"){
+        console.log(file.name);
+        const lector = new FileReader();
+        lector.onload = (event) =>{
+            src_imagen_producto = event.target.result;
+            document.querySelector("#imagen-file").src = src_imagen_producto;
+        }
+        lector.readAsDataURL(file);
+    }
 }

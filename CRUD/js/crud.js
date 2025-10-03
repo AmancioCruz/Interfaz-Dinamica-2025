ef91d88e03c8abdf516f2edbbd57e3016e4cd828
´amancio´ <amancio@gmail.com>
2025-10-03 12:46:55 -0600

    [agrega]funcion de eliminar en crud

--- CRUD/js/crud.js
+++ CRUD/js/crud.js
@@ -7,24 +7,46 @@ const obtener_datos_formulario = (event) => {
     console.log("funcion para obtener datos");
     const datos_formulario = new FormData(formulario); //lee el formulario y obtiene todo el fomrulario
     const datos = Object.fromEntries(datos_formulario.entries()); //convertminos en objeto 
-    
-    //validar que le usuario ingrese datos en el
-    //formulario mostrandoile en la vista un mensaje de error
-    //si el usuario no ingreso nada, que en la vista aprezca un mensaje de que debe ingresar datos
-    //una vez validado que el usuario ingreso datos crear la tarjeta con el elemento que se agrego al arreglo
-    //solo el ultimo
-    //es mostrae '1- Nombre del juego'
-    
-    //se agregan los videojuegos nuevos a un arreglo de vidoejuegos
-    videojuegos.push(new Videojuego(
-        videojuegos.length + 1, datos.titulo, datos.descripcion
-    ));
+    try {
+        if (!datos.titulo || !datos.descripcion) {
+            throw new Error('No se han ingresado los datos en el formulario');
+        }
+        //aqui hacer la logica para agregar a los videojuegos 
+        videojuegos.push(new Videojuego(
+            videojuegos.length + 1, datos.titulo, datos.descripcion
+        ));
+        if (videojuegos.length >= 0) {
+            const ultimo_videojuego = videojuegos[videojuegos.length - 1];
+            crearElementoVista(ultimo_videojuego.titulo, ultimo_videojuego.descripcion);
+        }
 
-    crearElementoVista(videojuegos[0].titulo, videojuegos[0].descripcion)
+        eliminar_videojuego(videojuegos.length);
 
-    console.log(videojuegos);
-    //const juego = new Videojuego();
+    } catch (error) {
+        document.querySelector("#mensaje_error").hidden = false;
+        document.querySelector("#mensaje_error").textContent = error.message;
+    }
 };
+
+function eliminar_videojuego(id) {
+
+    //necesito recibir el id del elemento que quiero eliminar
+    const videojuego_a_eliminar = videojuegos.filter(videojuego => videojuego.id === id);
+    try {
+        if(!videojuego_a_eliminar) {
+            throw new Error('no se puede eliminar el juego, ya que no existe');
+        }
+        //areglo =[1,2,3,4,5,6,7];
+        //arreglo.splice(0,2); va a eliminar el 1 y 2
+        //        splice(arreglo[0], cantidad de elentos a borrar)
+        //arreglo.splice(1,3) va a borrar el 2,3,4
+        videojuegos.splice();
+        //sirve para eliminar un elemento de un arreglo indice, cantidad de elementos a borrar
+        console.log('puedo eliminar el juego');
+    }catch(error){
+        console.log(error);
+    }
+}
 
 const crearElementoVista = (titulo_videojuego, descripcion_videojuego) => {
     const elemento = document.createElement('div');
@@ -53,5 +75,4 @@ const crearElementoVista = (titulo_videojuego, descripcion_videojuego) => {
 
 document.querySelector("#boton_agregar").addEventListener('click', () => {
     obtener_datos_formulario();
-    crearElementoVista();
 })

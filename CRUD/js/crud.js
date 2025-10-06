7776f3b986b588f1f5d6d52bfc56d500b537ee96
Amancio TC <Amancio.torres.cruz@gmail.com>
2025-10-06 11:38:19 -0600

    [Agrega]Arreglo de videojeugos

--- CRUD/js/crud.js
+++ CRUD/js/crud.js
@@ -1,7 +1,23 @@
 import { Videojuego } from "./definiciones.js";
 const formulario = document.querySelector("#formulario_agregar");
 const contenedor_elementos = document.querySelector('#contenedor_elementos_juegos');
-let videojuegos = [];
+let videojuegos = [
+    new Videojuego(1, "Pokémon Rojo y Azul", "Los juegos originales donde comenzó la aventura Pokémon en la región de Kanto."),
+    new Videojuego(2, "Pokémon Oro y Plata", "Introducen la región de Johto, los Pokémon de segunda generación y el ciclo de día y noche."),
+    new Videojuego(3, "Pokémon Rubí y Zafiro", "Presentan la región de Hoenn y las batallas dobles como nueva mecánica."),
+    new Videojuego(4, "Pokémon Diamante y Perla", "Llevan la serie al Nintendo DS con la región de Sinnoh y gráficos mejorados."),
+    new Videojuego(5, "Pokémon Negro y Blanco", "Se desarrollan en la región de Teselia con una historia más profunda y nuevos Pokémon."),
+    new Videojuego(6, "Pokémon X y Y", "Primeros juegos con gráficos totalmente en 3D, ambientados en la región de Kalos."),
+    new Videojuego(7, "Pokémon Sol y Luna", "Ambientados en Alola, reemplazan los gimnasios por los desafíos de las islas."),
+    new Videojuego(8, "Pokémon Espada y Escudo", "Introducen la región de Galar, las formas Dinamax y un área silvestre abierta."),
+    new Videojuego(9, "Pokémon Leyendas: Arceus", "Rompe con la fórmula tradicional con exploración abierta en la antigua Sinnoh."),
+    new Videojuego(10, "Pokémon Escarlata y Púrpura", "Primer juego completamente de mundo abierto ambientado en la región de Paldea.")
+];
+//CRUD
+//Crear elementos
+//Leer elementos
+//Actualizar elementos
+//Eliminar elementos 
 
 const obtener_datos_formulario = (event) => {
     console.log("funcion para obtener datos");
@@ -33,7 +49,7 @@ function eliminar_videojuego(id) {
     //necesito recibir el id del elemento que quiero eliminar
     const videojuego_a_eliminar = videojuegos.filter(videojuego => videojuego.id === id);
     try {
-        if(!videojuego_a_eliminar) {
+        if (!videojuego_a_eliminar) {
             throw new Error('no se puede eliminar el juego, ya que no existe');
         }
         //areglo =[1,2,3,4,5,6,7];
@@ -43,7 +59,7 @@ function eliminar_videojuego(id) {
         videojuegos.splice();
         //sirve para eliminar un elemento de un arreglo indice, cantidad de elementos a borrar
         console.log('puedo eliminar el juego');
-    }catch(error){
+    } catch (error) {
         console.log(error);
     }
 }

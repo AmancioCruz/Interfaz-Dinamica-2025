71b2827ab5c5e9e247681aba6d36cdefe3cf5c69
Amancio TC <Amancio.torres.cruz@gmail.com>
2025-11-05 11:17:40 -0700

    [Agrega] se agrega el canvas

--- Canvas/js/canvas.js
+++ Canvas/js/canvas.js
@@ -1,7 +1,21 @@
-import { Cuadrado } from "./figuras.js";
+import { Circulo, Cuadrado, Linea } from "./figuras.js";
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 let trazoIniciado = false;
+let posicionesCursor = {
+    iniciales: {
+        x: '',
+        y: ''
+    },
+    actuales: {
+        x: '',
+        y: ''
+    },
+    finales: {
+        x: '',
+        y: ''
+    }
+}
 
 /*
 ctx.strokeStyle = "black";
@@ -24,10 +38,48 @@ ctx.arc(400, 400, 100, 0, Math.PI);
 ctx.fill();
 ctx.stroke();*/
 
-canvas.addEventListener('click',(event)=>{
-    const figura = new Cuadrado(event.offsetX, event.offsetY, 100,100, "blue", "green", 5);
+
+canvas.addEventListener('mousedown', (event) => {
+    console.log('Se hizo click', registrarPosicionCursor(event));
+    trazoIniciado = true;
+    posicionesCursor.iniciales = registrarPosicionCursor(event);
+});
+
+canvas.addEventListener('mousemove', (event) => {
+    //console.log('el cursor esta sobre el canvas', registrarPosicionCursor(event));
+    /*if (trazoIniciado) {
+        posicionesCursor.actuales = registrarPosicionCursor(event);
+
+        const linea = new Linea(posicionesCursor.iniciales.x, posicionesCursor.iniciales.y,
+            posicionesCursor.actuales.x, posicionesCursor.actuales.y, 'black', 5);
+        linea.dibujar(ctx);
+
+        posicionesCursor.iniciales = registrarPosicionCursor(event);
+    }*/
+});
+
+canvas.addEventListener('mouseup', (event) => {
+    console.log('se dejo de hacer click', registrarPosicionCursor(event));
+    posicionesCursor.finales = registrarPosicionCursor(event);
+
+    /*const figura = new Cuadrado(posicionesCursor.iniciales.x, posicionesCursor.iniciales.y,
+        posicionesCursor.finales.x, posicionesCursor.finales.y, "blue", "green", 5);
+    figura.dibujar(ctx);*/
+
+    const figura = new Circulo(posicionesCursor.iniciales.x, posicionesCursor.iniciales.y, 
+        posicionesCursor.finales.x, posicionesCursor.finales.y, 'red', 'blue',5);
     figura.dibujar(ctx);
-})
+
+    trazoIniciado = false;
+
+    //como hacer para dibujar un cuadrado haciendo click y arrastrando 
+    //y que se cree del tamaño en donde suelto el click
+});
+
+function registrarPosicionCursor(event) {
+    const posicion = { x: event.offsetX, y: event.offsetY };
+    return posicion;
+}
 
 function obtenerPosicionCursor(event) {
     if (trazoIniciado) {
--- Canvas/js/figuras.js
+++ Canvas/js/figuras.js
@@ -1,18 +1,22 @@
 class Figura {
-    constructor(posicion_x, posicion_y, color_linea, color_relleno, grozor_linea) {
-        this.posicion_x = posicion_x;
-        this.posicion_y = posicion_y;
+    constructor(inicio_x, inicio_y, fin_x, fin_y, color_linea, color_relleno, grozor_linea) {
+        this.inicio_x = inicio_x;
+        this.inicio_y = inicio_y;
+        this.fin_x = fin_x;
+        this.fin_y = fin_y;
+        this.posicion_x = Math.min(inicio_x, fin_x);
+        this.posicion_y = Math.min(inicio_y, fin_y);
         this.color_linea = color_linea;
-        this.grozor_linea = grozor_linea;
         this.color_relleno = color_relleno;
+        this.grozor_linea = grozor_linea;
     }
 }
 
 export class Cuadrado extends Figura {
-    constructor(posicion_x, posicion_y, alto, ancho, color_linea, color_relleno, grozor_linea) {
-        super(posicion_x, posicion_y, color_linea, color_relleno, grozor_linea);
-        this.alto = alto;
-        this.ancho = ancho;
+    constructor(inicio_x, inicio_y, fin_x, fin_y, color_linea, color_relleno, grozor_linea) {
+        super(inicio_x, inicio_y, fin_x, fin_y, color_linea, color_relleno, grozor_linea);
+        this.ancho = Math.abs(fin_x - inicio_x);
+        this.alto = Math.abs(fin_y - inicio_y);
     }
 
     dibujar(ctx) {
@@ -24,5 +28,44 @@ export class Cuadrado extends Figura {
         ctx.fillRect(this.posicion_x, this.posicion_y, this.ancho, this.alto);
         ctx.strokeRect(this.posicion_x, this.posicion_y, this.ancho, this.alto);
     }
-
 }
+
+export class Circulo extends Figura {
+    constructor(inicio_x, inicio_y, fin_x, fin_y, color_linea, color_relleno, grozor_linea) {
+        super(inicio_x, inicio_y, fin_x, fin_y, color_linea, color_relleno, grozor_linea);
+        this.radio = Math.sqrt((fin_x - inicio_x) ** 2 + (fin_y - inicio_y) ** 2);
+    }
+
+    dibujar(ctx) {
+        ctx.beginPath();
+        ctx.strokeStyle = this.color_linea;
+        ctx.lineWidth = this.grozor_linea;
+        ctx.fillStyle = this.color_relleno;
+
+        ctx.arc(this.inicio_x, this.inicio_y, this.radio, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.stroke();
+    }
+}
+
+export class Linea extends Figura {
+    constructor(inicio_x, inicio_y, fin_x, fin_y, color_linea, grozor_linea) {
+        super(inicio_x, inicio_y, fin_x, fin_y, color_linea, null, grozor_linea);
+    }
+
+    dibujar(ctx) {
+        ctx.beginPath();
+        ctx.lineCap = "round";
+        ctx.lineJoin = "round";
+        ctx.strokeStyle = this.color_linea;
+        ctx.lineWidth = this.grozor_linea;
+
+        ctx.moveTo(this.inicio_x, this.inicio_y);
+        ctx.lineTo(this.fin_x, this.fin_y);
+        ctx.stroke();
+    }
+}
+
+//definir una clase que herede de figura pero que sea propia
+//no triangulos, ni figuras geometricas sencillas
+//puede ser una estrella, un corazon, una patita de perro 
